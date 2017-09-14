aa6a1e819c6ad1ce1dc41e048594e4670dc205d7
Robert Docter <rd+git@figo.io>
2017-09-14 15:02:42 +0200

    add execFile endpoint

---
+++ src/execFile-endpoint.ts
@@ -0,0 +1,59 @@
+import * as rx from './abstract-rx';
+import { execFile, ExecFileOptions, ChildProcess } from 'child_process';
+import RxEndpoint from './rx-endpoint';
+
+export class ExecFileEndpoint implements RxEndpoint<string> {
+  private subProcess: ChildProcess;
+
+  private file: string;
+  private args: string[];
+  private options: ExecFileOptions;
+
+  public input: rx.Subject<string>;
+  public output: rx.Subject<string>;
+
+  public static command(file: string, args: string[] = [], options: ExecFileOptions = {}): ExecFileEndpoint {
+    return new ExecFileEndpoint(file, args, options);
+  }
+
+  constructor(file: string, args: string[], options: ExecFileOptions) {
+    this.file = file;
+    this.args = args;
+    this.options = options;
+    this.input = rx.Observable.create((observer: rx.Observer<string>) => {
+      const errorStack: any[] = [];
+      this.subProcess = execFile(this.file, this.args, this.options);
+      this.subProcess.stdout.on('data', stuff => {
+        observer.next('' + stuff);
+      });
+      this.subProcess.stdout.on('error', (error: any) => {
+        observer.error(error);
+      });
+      this.subProcess.on('close', (status, signal) => {
+        if (!(status == 0)) {
+          observer.error(errorStack.concat({ status: status, signal: signal }));
+        } else if (errorStack.length) {
+          observer.error(errorStack);
+        } else {
+          observer.complete();
+        }
+      });
+      this.subProcess.on('error', (_data: any) => {
+        errorStack.push(_data);
+      });
+    });
+    this.output = new rx.Subject();
+    this.output.subscribe(
+      (a) => {
+        this.subProcess.stdin.write(a);
+      },
+      (e) => { /* */ },
+      () => {
+        this.subProcess.stdin.end();
+      }
+    );
+  }
+
+}
+
+export default ExecFileEndpoint;
---
+++ test/execFile-endpoint-test.ts
@@ -0,0 +1,89 @@
+import ExecFileEndpoint from '../src/execFile-endpoint';
+import * as rx from '../src/abstract-rx';
+
+import { assert } from 'chai';
+import * as path from 'path';
+
+function readAssert(rxep: ExecFileEndpoint, done: any): void {
+  function flatten<T>(arr: T[][]): T[] {
+    const tmp = Array.prototype.concat(...arr);
+    return tmp;
+  }
+  const checkArray: string[] = [];
+  rxep.input.subscribe(data => {
+    checkArray.push(data);
+  }, (error) => {
+    assert.fail('ERROR should not received', error);
+  }, () => {
+    assert.deepEqual(
+      flatten(checkArray.map(el => {
+        return String(el).split('\n');
+      }))
+        .filter(el => el !== '')
+        .map(n => parseInt(n, 10)), [1, 2, 3, 4, 5, 6]
+    );
+    done();
+  });
+}
+
+['rxjs', null].forEach(rxName => {
+
+  describe(`execFile plugin ${rxName}`, () => {
+    before(done => {
+      rx.inject(rxName);
+      done();
+    });
+
+    it('can error', done => {
+      const eep = ExecFileEndpoint.command('/wtf/wtf');
+      eep.input.subscribe((data: any) => {
+        assert.fail('never called');
+      }, (error) => {
+        assert.equal((error[0] as any).code, 'ENOENT');
+        const status = (error[1] as any).status;
+        // node 6 return -2
+        // node 0.10 return -1
+        assert.isTrue(status < 0);
+        done();
+      }, () => {
+        assert.fail('never called');
+      });
+    });
+
+    it('can exit code', done => {
+      const eep = ExecFileEndpoint.command(process.execPath, ['-e', 'process.exit(47)']);
+      eep.input.subscribe((data: any) => {
+        assert.fail('never called');
+      }, (data) => {
+        assert.equal((data[0] as any).status, 47);
+        done();
+      }, () => {
+        assert.fail('never called');
+      });
+    });
+
+    it('can receive if spawned.', done => {
+
+      const eep = ExecFileEndpoint.command(
+        process.execPath,
+        ['-e', '[1, 2, 3, 4, 5, 6].forEach(function(el) { console.log(el); })']);
+      readAssert(eep, done);
+    });
+
+    it('can span a echo server', done => {
+      const eep = ExecFileEndpoint.command(process.execPath,
+        [path.join('dist', 'test', 'echo-process.js')]);
+
+      readAssert(eep, () => {
+        done();
+      });
+
+      [1, 2, 3, 4, 5, 6].forEach((el) => {
+        eep.output.next(`${el}\n`);
+      });
+      eep.output.complete();
+    });
+
+  });
+
+});
